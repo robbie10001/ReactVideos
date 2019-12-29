7106757898ad0b6a7b91a4545885966ddb68d85e
robbie10001/rn <robbie_c1000@hotmail.com>
2019-12-29 11:51:14 +1100

    event handlers

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,8 +2,28 @@ import React from "react";
 import App from "./App";
 
 class SearchBar extends React.Component {
+    state = { term: ""}; 
+
+    onInputChange = () => {
+
+    };
+
     render() {
-        return <div>SearchBar</div>;
+        return (
+        
+        <div className="search-bar ui segment">
+            <form className="ui form">
+                <div className="field">
+                    <label>Video Search</label>
+                    <input 
+                    type="text" 
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                    />
+                </div>
+            </form>
+        </div>
+        );
     }
 }
 
