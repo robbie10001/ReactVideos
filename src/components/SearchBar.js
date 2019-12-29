21975d70486f56e85d08cc034cb532ba48dd1cda
robbie10001/rn <robbie_c1000@hotmail.com>
2019-12-29 11:38:51 +1100

    first bit of boilerplate for the app build

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "./App";
+
+class SearchBar extends React.Component {
+    render() {
+        return <div>SearchBar</div>;
+    }
+}
+
+export default SearchBar; 
