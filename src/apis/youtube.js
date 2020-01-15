e842f05399ae9f9b55679c4d61541e1e4bf0c57a
Robbie Colborne <robbie_c1000@hotmail.com>
2020-01-15 19:54:54 +1100

    accessing the youtube Api

---
+++ src/apis/youtube.js
@@ -0,0 +1,7 @@
+
+//We have defined this variable like this so we can't change it.
+const KEY = "AIzaSyD4ZeoKNIOW3N3qpKMJRfJuU8dtLKRQOgM";
+//this key is going to be used inside the broswer. 
+//This means that when a user accesses our application, we are sending them this api key. 
+//this user could easily get access to our api key. 
+//in our case, we don't really care, it's supposed to be publicly available. 
