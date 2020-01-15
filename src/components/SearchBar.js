2bac935f565792339ea9385cb6c4cf105d4081a8
Robbie Colborne <robbie_c1000@hotmail.com>
2020-01-15 19:38:53 +1100

    handling form submittal

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,14 +6,29 @@ import App from "./App";
 class SearchBar extends React.Component {
 //here we are initalising the state object. 
 //by saying term is an empty string. term could be anything. 
-//
+
     state = { term: ""}; 
 
 //we define out method for when a user changes input in the search bar.
 //because it's an event callback we define this function using a fat arrow. 
-    onInputChange = () => {
+//this event handler taken and event object (event)
+//we want to take the value of the user input out of this event object, 
+//and assign it to our state property. 
+//this will allow us to type in our video search bar. 
+    onInputChange = event => {
+//we are calling this.setState 
+// and we update the value of our term property,
+//with the value from event.target.value
+        this.setState({term: event.target.value});
 
-
+    };
+//we create a method called onFormSumbit. 
+//this is an arrow function because this is a callback, 
+//that we are going to assign some child element. 
+    onFormSubmit = event => {
+        //we are going to make sure the browser does not automatically try to submit the form, 
+        //anytime the user actually passed data in. 
+        event.preventDefault();
     };
 
 //render method 
@@ -22,11 +37,18 @@ class SearchBar extends React.Component {
         return (
         
         <div className="search-bar ui segment">
-            <form className="ui form">
+           
+           {/* we need a callback function on our form. 
+            so that we can watch for anytime our form gets submitted.
+            onSubmit is a specially named property. 
+            Anytime the user submit the form we call the callback method
+            of this.onFormSubmit
+            */}
+            <form onSubmit={this.onFormSubmit} className="ui form">
                 <div className="field">
                     <label>Video Search</label>
-                    {/* 1.a we have made input into a controlled element 
-                    we are tell the input that its value is coming from this.state.term*/}
+                    {/* we have made input into a controlled element 
+                    we tell the input that its value is coming from this.state.term*/}
                     <input 
                     type="text" 
                     value={this.state.term}
@@ -35,8 +57,7 @@ class SearchBar extends React.Component {
                      has typed in new input, update our state and get the input to rerender
                     our input value we be completely fixed.
                     //The onChange is very special, if we pass a callback handler, to the on
-                    //onChange prop it will be update everytime our input changes 
-                    */
+                    //onChange prop it will be update everytime our input changes*/
                     onChange={this.onInputChange}
                     />
                 </div>
