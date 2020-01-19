f72dc66c295dbef75abfda98171b00f33f4f036b
Robbie Colborne <robbie_c1000@hotmail.com>
2020-01-20 09:39:54 +1100

    Rendering video list with notes

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
+
 const VideoItem = (props) => {
+
+//we actually want to render out a list of videos. We want multiple video items when our application is rendered on screen. 
+//every VideoItem component is going to recieve, a seperate video object.
+//each VideoItem is will be customised to display a slightly different video. 
     return <div>Video Item</div>
 };
 
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import VideoItem from "./VideoItem"; 
 
+//rather than referencing props.videos.length like we had in the last video
+//we can destructure this using ES2015 syntax, just the videos property out of the props object. 
 
+//we are going to map over this videos array. 
+//the map method creates a new array with the results of calling a function for every array element. 
+//we will then render one single video item for every item in our videos component. 
+//we create a functioal component that takes the array of videos
 const VideoList = ({ videos }) => {
+//
+
+//the videos.map(video)- this is an array taken from the app component. 
+//we then map over it. We taken the inner function, that we pass to map a single argument called video. 
+//inside of the videos array, there are many objects which we are refering to video.
+//The inner function will be called one time for every object inside videos array. 
    const renderedList = videos.map((video) => {
+//We then return a VideoItem component. 
+//when we map of the Videos array it returns a brand new array, which contains a list of different VideoItem components.
+//we assign that list of VideoItem components to a new variable called renderedList.
+//renderedList is the result of mapping over the videos array. 
         return <VideoItem />; 
     });
 //instead of returning props.videos within our div, we can destructure the videos property out of the below object.
