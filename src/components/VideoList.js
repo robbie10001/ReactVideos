54eb34bba37612c13f4ea762244e8236eec994e6
Robbie Colborne <robbie_c1000@hotmail.com>
2020-01-20 09:16:54 +1100

    rendering a list of videos

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import VideoItem from "./VideoItem"; 
 
-const VideoList = props => {
-    return <div>{props.videos.length}</div>
+
+const VideoList = ({ videos }) => {
+   const renderedList = videos.map((video) => {
+        return <VideoItem />; 
+    });
+//instead of returning props.videos within our div, we can destructure the videos property out of the below object.
+    return <div>{renderedList}</div>
 };
 
 export default VideoList; 
