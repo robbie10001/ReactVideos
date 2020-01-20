f1f5122e104f24b6a05183c321f57c69d63cf84c
Robbie Colborne <robbie_c1000@hotmail.com>
2020-01-20 18:23:29 +1100

    rendering video thumbnails

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
 
-const VideoItem = (props) => {
+//The props withi VideoItem now contains video within it. 
+const VideoItem = ( { video }) => {
 
 //we actually want to render out a list of videos. We want multiple video items when our application is rendered on screen. 
 //every VideoItem component is going to recieve, a seperate video object.
 //each VideoItem is will be customised to display a slightly different video. 
-    return <div>Video Item</div>
+//in our img we are finding a thumbnail for each of our five videos. 
+return (
+    <div>
+        <img src={video.snippet.thumbnails.medium.url} />
+        {video.snippet.title}
+    </div>
+    );
 };
+
 
 export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -9,18 +9,21 @@ import VideoItem from "./VideoItem";
 //we will then render one single video item for every item in our videos component. 
 //we create a functioal component that takes the array of videos
 const VideoList = ({ videos }) => {
-//
 
 //the videos.map(video)- this is an array taken from the app component. 
 //we then map over it. We taken the inner function, that we pass to map a single argument called video. 
 //inside of the videos array, there are many objects which we are refering to video.
 //The inner function will be called one time for every object inside videos array. 
-   const renderedList = videos.map((video) => {
+   const renderedList = videos.map(video => {
 //We then return a VideoItem component. 
 //when we map of the Videos array it returns a brand new array, which contains a list of different VideoItem components.
 //we assign that list of VideoItem components to a new variable called renderedList.
 //renderedList is the result of mapping over the videos array. 
-        return <VideoItem />; 
+        //we add a property called video and pass it the video that we are looping over.
+        //this means in our VideoItem.js our VideoItem cointains a video property.
+        //this has all the information about the particular video that we want to show as a list. 
+        //the video object {video} is what we got back from the yotube API 
+        return <VideoItem video={video} />; 
     });
 //instead of returning props.videos within our div, we can destructure the videos property out of the below object.
     return <div>{renderedList}</div>
