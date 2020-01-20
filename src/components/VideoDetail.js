a2bdea94576f2c2214b44ef8433ffafc3de496d0
Robbie Colborne <robbie_c1000@hotmail.com>
2020-01-21 08:58:04 +1100

    styling

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,10 +1,25 @@
 import React from 'react'
-
+//here we are creating the videoDetail component. 
+//we are passing down a prop (video) which is the currently selected video on the apps state property. 
+//this will show a video player and a title and description underneath the player. 
+//below we have created a functional component. 
+//VideoDetail is called with the props object of ({video})
+//by defining our props argument as video, we no longer need to write code throughout our function as below,
+//props.video, we can instead just reference video. 
+//now that we have this component, we can render it within our render method within our app  component. 
 const VideoDetail = ({ video }) => {
     if (!video) {
         return <div>Loading...</div>;
     }
-    return <div>{video.snippet.title}</div>
+    return (
+        <div>
+            <div className="ui segment">
+                <h4 className="ui header">{video.snippet.title}</h4>
+                <p>{video.snippet.description}</p>
+            </div>
+        </div>
+    
+    );
 };
 
 export default VideoDetail;
